32ca50c534fe32fb60e85200bb838caa8803024b
Quantum Cucumber <null>
2021-09-04 13:17:00 +1000

    Filter through spaces

--- src/pages/Client.js
+++ src/pages/Client.js
@@ -5,7 +5,6 @@ import { useEffect, useState } from "react";
 import { Button } from "../components/interface";
 import { Icon } from "@mdi/react";
 import { mdiCog, mdiHomeVariant } from "@mdi/js";
-import { RoomState } from "matrix-js-sdk";
 
 function Client() {
     // On first load, start syncing. Once synced, change state to reload as client
@@ -62,14 +61,16 @@ function RoomList() {
 
     var rooms = [];
     global.matrix.getRooms().forEach((room) => {
-        const key = room.roomId;
-        rooms.push(
-            <div
-                className={"room " + (currentRoom === key ? "room--selected" : "")}
-                key={key}
-                onClick={() => { selectRoom(key) }}
-            >{room.name}</div>
-        );
+        if (!room.isSpaceRoom()) {
+            const key = room.roomId;
+            rooms.push(
+                <div
+                    className={"room " + (currentRoom === key ? "room--selected" : "")}
+                    key={key}
+                    onClick={() => { selectRoom(key) }}
+                >{room.name}</div>
+            );
+        }
     });
 
     return rooms;
@@ -83,11 +84,33 @@ function GroupList() {
             className={"group group--default " + (currentGroup === "home" ? "group--selected" : "")}
             key="home" onClick={() => { selectGroup("home") }}
         >
-            <Icon path={mdiHomeVariant} color="var(--text-greyed)" size="100%" />
+            <Icon path={mdiHomeVariant} color="var(--text)" size="100%" />
         </div>)
     ];
 
+    global.matrix.getRooms().forEach((room) => {
+        if (room.isSpaceRoom()) {
+            const key = room.roomId;
+            const icon = room.getAvatarUrl(global.matrix.getHomeserverUrl(), 96, 96, "crop");
+            const image = icon ? 
+                <img className="group__icon" src={icon} alt={room.name} /> : 
+                <div className="group__icon">{acronym(room.name)}</div>;
+
+            groups.push(
+                <div
+                    className={"group " + (currentGroup === key ? "group--selected" : "")}
+                    key={key}
+                    onClick={() => { selectGroup(key) }}
+                >{image}</div>
+            );
+        }
+    })
+
     return groups;
+}
+
+function acronym(text, len=3) {
+    return text.match(/\b([a-z0-9])/gi).slice(0, len).join("").toUpperCase()
 }
 
 export default Client;
