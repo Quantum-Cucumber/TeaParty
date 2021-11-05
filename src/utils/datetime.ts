0c2e24376d6498439bcff1698160c4c7c47340db
Quantum Cucumber <quantumcucumber@protonmail.com>
2021-11-06 10:53:51 +1100

    Typescript to utils/datetime

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -1,11 +1,13 @@
 import { DateTime } from "luxon";
 
-function _jsDateToLocal(date) {
+import type { MatrixEvent } from "matrix-js-sdk";
+
+function _jsDateToLocal(date: Date): DateTime {
     /* Convert a utc js date object into a luxon DateTime in the browser's local time */
-    return DateTime.fromJSDate(date, "UTC").toLocal();
+    return DateTime.fromJSDate(date, {zone: "UTC"}).toLocal();
 }
 
-function _relativeDate(date, format = DateTime.DATE_SHORT) {
+function _relativeDate(date: DateTime, format = DateTime.DATE_SHORT): string {
     /* Return today/yesterday/a date string by comparing to the current date */
     // Check if the next message is today
     const now = DateTime.local();
@@ -22,12 +24,12 @@ function _relativeDate(date, format = DateTime.DATE_SHORT) {
     }}
 
 
-export function dateToTime(date) {
+export function dateToTime(date: Date): string {
     const dt = _jsDateToLocal(date);
     return dt.toLocaleString(DateTime.TIME_24_SIMPLE);
 }
 
-export function messageTimestamp(date) {
+export function messageTimestamp(date: Date) {
     const dt = _jsDateToLocal(date);
     const dateStr = _relativeDate(dt);
     const timeStr = dateToTime(date);
@@ -39,28 +41,28 @@ export function messageTimestamp(date) {
     }
 }
 
-export function messageTimestampFull(date) {
+export function messageTimestampFull(date: Date) {
     const dt = _jsDateToLocal(date);
     const format = {...DateTime.DATETIME_FULL, timeZoneName: undefined};  // Remove the timezone text
     return dt.toLocaleString(format);
 }
 
-export function dateToWeekdayDate(date) {
+export function dateToWeekdayDate(date: Date) {
     const dt = _jsDateToLocal(date);
     return dt.toLocaleString(DateTime.DATE_HUGE);
 }
 
-export function dateToDateStr(date) {
+export function dateToDateStr(date: Date) {
     const dt = _jsDateToLocal(date);
     return dt.toLocaleString(DateTime.DATE_FULL);
 }
 
 
-export function dayBorder(nextMsg, lastMsg) {
-    if (!nextMsg || !lastMsg) {return null}
+export function dayBorder(nextMsg?: MatrixEvent, lastMsg?: MatrixEvent) {
+    if (!nextMsg?.getDate() || !lastMsg?.getDate()) {return null}
     // Get local DT from the utc date
-    const nextLocal = _jsDateToLocal(nextMsg.getDate());
-    const lastLocal = _jsDateToLocal(lastMsg.getDate());
+    const nextLocal = _jsDateToLocal(nextMsg.getDate()!);
+    const lastLocal = _jsDateToLocal(lastMsg.getDate()!);
     // Check whether dates are the same
     if (nextLocal.day !== lastLocal.day) {
         return _relativeDate(nextLocal, DateTime.DATE_FULL);
@@ -69,7 +71,7 @@ export function dayBorder(nextMsg, lastMsg) {
     }
 }
 
-export function msToDate(ms) {
+export function msToDate(ms: number): string {
     if (!ms) {return ""};
 
     const dt = DateTime.fromMillis(ms, {locale: "UTC"}).toLocal();
